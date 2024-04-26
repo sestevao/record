3dc9bf62e4cbdebeece3e880e45e126f850cb90f
Susana <9211533+sestevao@users.noreply.github.com>
2024-04-26 15:14:28 +0100

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
-import { createServer} from 'node:http'
 import { createReadStream } from 'node:fs'
+import { createServer } from 'node:http'
 import { Readable, Transform } from 'node:stream'
-import { WritableStream, TransformStream } from 'node:stream/web'
-import { setTimeout } from 'node:timers/promises'
+import { TransformStream, WritableStream } from 'node:stream/web'
 
 import csvtojson from 'csvtojson'
-
 const PORT = 3000
 
 // curl -N localhost:3000
@@ -26,38 +24,49 @@ createServer(async (request, response) => {
 
     const csvArchive = './animeflv.csv';
     let items = 0
-    request.once("close", (_) =>
+
+    request.once("close", _ =>
         console.log(`connection was closed!`, items)
     )
 
-    await Readable.toWeb(createReadStream(csvArchive))
-        // passo a passo que cada item individual vai trafegar
-        .pipeThrough(Transform.toWeb(csvtojson()))
-        .pipeThrough(new TransformStream({
-            transform(chunk, controller) {
-                const data = JSON.parse(Buffer.from(chunk))
-                const mappedData = {
-                    title: data.title,
-                    description : data.description,
-                    url_anime: data.url_anime,
+    try{
+        response.writeHead(200, headers)
+
+        await Readable.toWeb(createReadStream(csvArchive))
+            // passo a passo que cada item individual vai trafegar
+            .pipeThrough(Transform.toWeb(csvtojson()))
+            .pipeThrough(new TransformStream({
+                transform(chunk, controller) {
+                    const data = JSON.parse(Buffer.from(chunk))
+                    const mappedData = {
+                        title: data.title,
+                        description : data.description,
+                        url_anime: data.url_anime,
+                    }
+                    //quebra de linha pois e um NDJSON
+                    controller.enqueue(JSON.stringify(mappedData).concat('\n'))
                 }
-                //quebra de linha pois e um NDJSON
-                controller.enqueue(JSON.stringify(mappedData).concat('\n'))
-            }
-        }))
-        //pipeTo e a ultima etapa
-        .pipeTo(new WritableStream({
-            async write(chunk) {
-                await setTimeout(1000)
-                items++
-                response.write(chunk)
-            },
-            close() {
-                response.end()
-            }
-        }))
+            }))
+            //pipeTo e a ultima etapa
+            .pipeTo(new WritableStream({
+                async write(chunk) {
+                    // await setTimeout(200)
+                    items++
+                    response.write(chunk)
+                },
+                close() {
+                    response.end()
+                }
+            }), {
+                signal: abortController.signal
+            })
+        } catch(error) {
+            if (!error.message.includes('abort')) 
+                throw error
+        }
+        // response.writeHead(200, headers)
+        // response.end('ok2')
 
-    response.writeHead(200, headers)
-    // response.end('ok2')
-
-}).listen(PORT).on('listening', _ => console.log(`server is running at ${PORT}`))
+})
+.listen(PORT)
+.on('listening', _ => console.log(`server is running at ${PORT}`))
